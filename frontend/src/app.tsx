f7963378396a60f02aef37f94bda39951f22c68f
monsterkrampe <monsterkrampe@users.noreply.github.com>
2022-08-29 18:20:06 +0200

    Build basic ui with mui

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import App from './components/app.tsx';
 
 const container = document.getElementById('app');
--- frontend/src/components/app.tsx
+++ frontend/src/components/app.tsx
@@ -1,9 +1,94 @@
 import * as React from 'react';
 
-import Graph from './graph.tsx';
+const { useState, useCallback } = React;
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Backdrop, Button, CircularProgress, CssBaseline, Container, Link, Typography, TextField } from '@mui/material';
+
+//import Graph from './graph.tsx';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const placeholder = `s(7).
+s(4).
+s(8).
+s(3).
+s(5).
+s(9).
+s(10).
+s(1).
+s(6).
+s(2).
+ac(7,c(v)).
+ac(4,6).
+ac(8,or(neg(1),7)).
+ac(3,and(or(7,neg(6)),2)).
+ac(5,4).
+ac(9,neg(7)).
+ac(10,and(neg(2),6)).
+ac(1,and(neg(7),2)).
+ac(6,neg(7)).ac(2,and(neg(9),neg(6))).`;
 
 function App() {
-  return <Graph />;
+  const [loading, setLoading] = useState(false);
+  const [code, setCode] = useState(placeholder);
+  const [graph, setGraph] = useState();
+
+  const submitHandler = useCallback(
+    () => {
+      setLoading(true);
+
+      fetch('http://localhost:8080/solve', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ code }),
+      })
+        .then(res => res.json())
+        .then(data => setGraph(data))
+        .finally(() => setLoading(false));
+      // TODO: error handling
+    },
+    [code],
+  );
+
+  console.log(graph);
+
+  return <ThemeProvider theme={darkTheme}>
+    <CssBaseline/>
+    <main>
+      <Typography variant="h2" component="h1" align="center" gutterBottom>
+        Solve your ADF Problem with Style!
+      </Typography>
+
+      <Container>
+        <TextField 
+          name="code" 
+          label="Put your code here:" 
+          helperText={<>For more info on the syntax, have a look <Link href="https://github.com/ellmau/adf-obdd" target="_blank" rel="noreferrer">here</Link>.</>} 
+          multiline 
+          fullWidth 
+          variant="filled" 
+          value={code}
+          onChange={(event) => { setCode(event.target.value); }}
+        />
+      </Container>
+      <Container maxWidth="xs">
+        <Button fullWidth variant="outlined" onClick={submitHandler}>Solve it!</Button>
+      </Container>
+    </main>
+
+    <Backdrop
+      open={loading}
+    >
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  </ThemeProvider>;
 }
 
 export default App;
