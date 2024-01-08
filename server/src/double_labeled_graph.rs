ellmau/adf-obdd

commit 530fb5c4e61650be2eea062704036a11c386f8bc

Fix clippy warnings

server/src/double_labeled_graph.rs
@@ -92,7 +92,7 @@ impl DoubleLabeledGraph {
             .iter()
             .enumerate()
             .filter(|(i, _)| node_indices.contains(i))
-            .filter(|(_, node)| !vec![Var::TOP, Var::BOT].contains(&node.var()))
+            .filter(|(_, node)| [Var::TOP, Var::BOT].contains(&node.var()))
             .map(|(i, &node)| (i, node.lo().value()))
             .map(|(i, v)| (i.to_string(), v.to_string()))
             .collect();
@@ -103,7 +103,7 @@ impl DoubleLabeledGraph {
             .iter()
             .enumerate()
             .filter(|(i, _)| node_indices.contains(i))
-            .filter(|(_, node)| !vec![Var::TOP, Var::BOT].contains(&node.var()))
+            .filter(|(_, node)| [Var::TOP, Var::BOT].contains(&node.var()))
             .map(|(i, &node)| (i, node.hi().value()))
             .map(|(i, v)| (i.to_string(), v.to_string()))
             .collect();

lib/src/obdd/vectorize.rs
@@ -23,5 +23,5 @@ where
     V: Deserialize<'de>,
 {
     let container: Vec<_> = serde::Deserialize::deserialize(des)?;
-    Ok(T::from_iter(container.into_iter()))
+    Ok(T::from_iter(container))
 }

commit 5e406bfe761c435151be8b7d66e1de80d92385a1

Fix bug introduced by 530fb5c

server/src/double_labeled_graph.rs
@@ -92,7 +92,7 @@ impl DoubleLabeledGraph {
             .iter()
             .enumerate()
             .filter(|(i, _)| node_indices.contains(i))
-            .filter(|(_, node)| [Var::TOP, Var::BOT].contains(&node.var()))
+            .filter(|(_, node)| ![Var::TOP, Var::BOT].contains(&node.var()))
             .map(|(i, &node)| (i, node.lo().value()))
             .map(|(i, v)| (i.to_string(), v.to_string()))
             .collect();
@@ -103,7 +103,7 @@ impl DoubleLabeledGraph {
             .iter()
             .enumerate()
             .filter(|(i, _)| node_indices.contains(i))
-            .filter(|(_, node)| [Var::TOP, Var::BOT].contains(&node.var()))
+            .filter(|(_, node)| ![Var::TOP, Var::BOT].contains(&node.var()))
             .map(|(i, &node)| (i, node.hi().value()))
             .map(|(i, v)| (i.to_string(), v.to_string()))
             .collect();